a2f723d4ec339e6c81460ce6f2cd34a77eb7cf15
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-11-23 14:41:49 -0500

    Added plugins

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import twitterScraper.tweetParser
 import twitterScraper.TwitterListener 
 from twitterScraper.scraper import *
 import tweepy
+from server import *
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-
+#testing
 def print_stuff():
 	for status in tweepy.Cursor(api.home_timeline).items(10):
 		process_or_store(status._json)
@@ -19,6 +20,6 @@ def print_stuff():
 
 
 if __name__ == "__main__":
-	print_stuff()
+	#print_stuff()
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
