091c74eed8a9cfbdc312ad6af3bd490b41a3576c
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-12-26 15:28:34 -0500

    Not using twitter scraper in main app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 import os
-import twitterScraper.tweetParser
-import twitterScraper.TwitterListener 
-from twitterScraper.scraper import *
+#import twitterScraper.tweetParser
+#import twitterScraper.TwitterListener 
+#from twitterScraper.scraper import *
 import tweepy
 from server import *
 
@@ -14,12 +14,12 @@ def hello_world():
 
 #testing
 def print_stuff():
-	for status in tweepy.Cursor(api.home_timeline).items(10):
-		process_or_store(status._json)
+	#for status in tweepy.Cursor(api.home_timeline).items(10):
+	#	process_or_store(status._json)
 
 
 
 if __name__ == "__main__":
-	print_stuff()
+	
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
