1c74f8ce9d71088c7fdc24943634f0c4785efc62
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-12-29 12:19:59 -0500

    Populating dashboard

--- static/js/script.js
+++ static/js/script.js
@@ -5,11 +5,22 @@ function getRSSFeedData() {
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
         success: function(response) {
-        	console.log("Got RSS Feed data!");
+            console.log("Got RSS Feed data!");
             console.log(response);
+            populateData(response.result);
 
         }
     });
 }
 
 getRSSFeedData();
+
+function populateData(data) {
+    for (var key in data) {
+        $("#noti-box").html($("#noti-box").html() + "<h1>" + key + "</h1>");
+        for (var i = 0; i < data[key].length; i++) {
+            $("#noti-box").html($("#noti-box").html() +"<div class = 'RSSAlert'>"+data[key][i]["title"]+data[key][i]["content"]+"</div><hr>");
+        }
+
+    }
+}
